221bfed18f5f0335fd5faf21d9cd58bb880bd6ad
Alex Guzhyk <aleksandr.guzhyk@gmail.com>
2019-04-23 15:41:07 +0300

    change directory and added new projects

---
+++ weather/App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <MapView
+        style = {styles.map}
+        region = {
+          {
+            latitude: 50.4183361,
+            longitude: 30.4849596,
+            latitudeDelta: 0.522,
+            longitudeDelta: 0.421
+          }
+        }
+        showsUserLocation = {
+          true
+        }
+        />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  map: {
+    flex: 1,
+  }
+});
